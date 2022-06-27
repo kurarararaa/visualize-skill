5d2b3ff0df3827aa58a331ba981e44353675083a
LOCALPSC\a-ohta <a-ohta@pscsrv.co.jp>
2022-06-27 18:30:58 +0900

    検索まわり作成中

---
+++ src/lib/header/Proficiency.svelte
@@ -0,0 +1,13 @@
+<Set chips={choices} let:chip filter bind:selected>
+  <Chip {chip} touch>
+    <Text>{chip}</Text>
+  </Chip>
+</Set>
+
+<script lang="ts">
+  import Chip, { Set, Text } from '@smui/chips';
+
+  let choices = ['★', '★★', '★★★'];
+  let selected = ['★', '★', '★'];
+</script>
+
---
+++ src/lib/header/Search.svelte
@@ -0,0 +1,58 @@
+<div class="columns margins">
+  <div>
+    <Textfield
+      class="shaped-outlined"
+      variant="outlined"
+      bind:value={valueA}
+      label="#タグやキーワードで検索"
+    >
+      <HelperText slot="helper">Helper Text</HelperText>
+    </Textfield>
+
+    <pre class="status">Value: {valueA}</pre>
+
+    <div class="flex">
+      <Proficiency></Proficiency>
+    </div>
+  </div>
+
+</div>
+
+<script lang="ts">
+  import Textfield from '@smui/textfield';
+  import HelperText from '@smui/textfield/helper-text';
+  import Proficiency from '$lib/header/Proficiency.svelte';
+
+
+  let valueA = '';
+
+
+</script>
+
+<style>
+  *
+    :global(.shaped-outlined
+      .mdc-notched-outline
+      .mdc-notched-outline__leading) {
+    border-radius: 28px 0 0 28px;
+    width: 28px;
+  }
+  *
+    :global(.shaped-outlined
+      .mdc-notched-outline
+      .mdc-notched-outline__trailing) {
+    border-radius: 0 28px 28px 0;
+  }
+  * :global(.shaped-outlined .mdc-notched-outline .mdc-notched-outline__notch) {
+    max-width: calc(100% - 28px * 2);
+  }
+  *
+    :global(.shaped-outlined.mdc-text-field--with-leading-icon:not(.mdc-text-field--label-floating)
+      .mdc-floating-label) {
+    left: 16px;
+  }
+  * :global(.shaped-outlined + .mdc-text-field-helper-line) {
+    padding-left: 32px;
+    padding-right: 28px;
+  }
+</style>
