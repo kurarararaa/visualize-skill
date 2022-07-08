f5ac7655e17d7124f529d3b6a1d1f4aa2dc34e26
PSChirano <k-hirano@pscsrv.co.jp>
2022-07-08 19:55:26 +0900

    🚧要員画面のヘッダにアイコン追加

--- src/lib/SkillCardList.svelte
+++ src/lib/SkillCardList.svelte
@@ -3,45 +3,39 @@
     <div class="card-container">
       <Card style="margin-bottom: 25px;">
         <Content style="width=95%; border-bottom: 1px dashed grey; margin: 0px 3% 25px 3%;"> 
-          <div>
-            <ImageList class="my-image-list-standard">
-              <Item>
-                <ImageAspectContainer>
-                  <Image
-                    src="..\static\favicon.png"
-                  />
-                </ImageAspectContainer>
-              </Item>
-            </ImageList>
-          </div>
-          <div>
-            やまだはなこ
-          </div>
-          <div>
-            山田花子
-          </div>
-          <div>
-            No.000317
-          </div>
-          <Actions>
-            <ActionIcons>
-              30歳
-            </ActionIcons>
-          </Actions>
-          <Actions>
-            <ActionIcons>
-              1990.01.25 生
-            </ActionIcons>
-          </Actions>
+          <LayoutGrid>
+            <Cell span={4}>
+              <img class="image-icon" src="{user.photoURL}" />
+            </Cell>
+            <Cell span={4}>
+              <div>
+                やまだはなこ
+              </div>
+              <div class="name-text">
+                山田花子
+              </div>
+              <div>
+                No.000317
+              </div>
+            </Cell>
+            <Cell span={4}>
+              <div>
+                30歳
+              </div>
+              <div>
+                1990.7.7生
+              </div>
+            </Cell>
+          </LayoutGrid>
         </Content>
 
         <div> 
-        <!-- カードリスト -->
-        {#each skills as skill}
-        <div  class="cardlist-row">
-          <SkillCard {...skill}/>
-        </div>
-        {/each}
+          <!-- カードリスト -->
+          {#each skills as skill}
+            <div  class="cardlist-row">
+              <SkillCard {...skill}/>
+            </div>
+          {/each}
         </div> 
       </Card>
       
@@ -55,6 +49,7 @@
     Actions,
     ActionIcons,
   } from '@smui/card';
+  import LayoutGrid, { Cell } from '@smui/layout-grid';
   import SkillCard from '$lib/SkillCard.svelte';
   import ImageList, {
     Item,
@@ -63,6 +58,7 @@
     Supporting,
     Label,
   } from '@smui/image-list';
+  import authStore from '../stores/authStore';
 
   let skills = [
     { name: 'Java', level: '★★☆', msg:'ある程度できる'},
@@ -74,11 +70,23 @@
     { name: 'Java', level: '★★☆', msg:'ある程度できる'},
     { name: 'Vue', level: '★★★', msg:'人に教えられる人に教えられる人に教えられる人に教えられる人に教えられる人に教えられる' },
     { name: 'Typescript', level: '★☆☆', msg:'初心者です' }
-  ];
+  ];  
+
+let user;
+authStore.subscribe(value => {
+  user = value.user;
+});
 </script>
 
 <style>
  .cardlist-row {
   margin: 0px 5% 25px 5%;
  }
+ .name-text {
+  font-size: xx-large;
+}
+.image-icon {
+  border-radius: 50%;
+  border: 1px solid gray;
+}
  </style>
