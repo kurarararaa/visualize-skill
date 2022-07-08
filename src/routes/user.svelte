bc5a1f2115e0e0f13e968da998b645a58c7c95a2
PSChirano <k-hirano@pscsrv.co.jp>
2022-07-08 19:14:39 +0900

    🚧要員画面用のページ追加

---
+++ src/routes/user.svelte
@@ -0,0 +1,9 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import SkillCardList from '$lib/SkillCardList.svelte';
+</script>
+
+<SkillCardList></SkillCardList>
