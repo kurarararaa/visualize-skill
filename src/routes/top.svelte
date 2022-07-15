27857cb20d12b13a017c572dbe4efb92034a9fe6
LOCALPSC\a-ohta <a-ohta@pscsrv.co.jp>
2022-07-15 19:56:03 +0900

    storeからスキル・ユーザ情報を取得

--- src/routes/top.svelte
+++ src/routes/top.svelte
@@ -1,9 +1,25 @@
 <script context="module" lang="ts">
 	export const prerender = true;
+	import Results from '$lib/Results.svelte';
 </script>
 
 <script lang="ts">
 	import Search from '$lib/Search.svelte';
+	import skills from '../stores/skillsStore';
+	import users from '../stores/usersStore';
+
+	let viewSkills;
+	let viewUsers;
+
+	skills.subscribe(value => {
+		console.log(value);
+		viewSkills = value;
+	});
+
+	users.subscribe(value => {
+		console.log(value);
+		viewUsers = value;
+	});
 </script>
 
 <div class="center">
